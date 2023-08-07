cdfcd7b7dc7f8b0607851d8046424d2f465f6840
ghdeo <goldencorn7@naver.com>
2023-08-07 20:50:34 +0900

    FE_0804

---
+++ day07/js/day01.js
@@ -0,0 +1,36 @@
+/**
+ * 1. 변수 선언
+ * let, var
+ * const
+ */
+
+// const name = 'ghdrlfehdwjs';
+// console.log('anem')
+
+
+let a = 10;
+let b = 'aaa';
+let c = 10.2;
+let d = 10 == 10;
+let e = null;
+let f;
+
+console.log(typeof a, 'a');
+console.log(typeof b, 'b');
+console.log(typeof c, 'c');
+console.log(typeof d, 'd');
+console.log(typeof e, 'e');
+console.log(typeof f, 'f');
+
+console.log(10 / 0, '10/0');
+console.log(-10 / 0, '-10/0');
+console.log('aa' / 10, 'aa/0');
+
+let g = [];
+let h = {};
+let i = function () {};
+
+console.log(typeof g);
+console.log(typeof h);
+console.log(typeof i);
+
